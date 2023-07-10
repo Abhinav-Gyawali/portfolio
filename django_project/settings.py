ed1a2e09467e352db21c514675758b14ee4386c1
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-07-10 20:03:51 +0545

    finally

--- django_project/settings.py
+++ django_project/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from pages import views
+from django.conf import settings
+from django.views.static import serve
+
 
 urlpatterns = [
     path('',views.index,name="home"),
@@ -8,4 +11,5 @@ urlpatterns = [
     path('services',views.services,name="services"),
     path('projects',views.projects,name="projects"),
     path('contact-me',views.contact,name="contact"),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
