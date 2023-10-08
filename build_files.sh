4d57d269d87f821c84b7aa304c6dbf66a5a29d54
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 17:43:06 +0545

    k ma hency hzhztdfiy7hrixhdjxijeixidhsux

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,10 @@ echo "BUILD STARTED"
 python3.9 -m pip install -r requirements.txt
 echo "BUID ENDED"
 echo "MIGRATION STARTED"
-python3.9 manage.py makemigrations --noinput --clear
+python3.9 manage.py makemigrations --noinput
 
 echo "MIGRATING"
-python3.9 manage.py migrate --noinput --clear
+python3.9 manage.py migrate --noinput
 echo "COLLECTING STATIC FILES"
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD SUCCESSFUL!"
