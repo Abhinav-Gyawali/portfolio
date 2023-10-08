1d2f2f1394bd170d704502644f936274ee6b10b3
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 09:54:43 +0545

    mersnsjsnns

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
