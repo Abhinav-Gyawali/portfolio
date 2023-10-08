ec20326d044d12aef0a6f5bfd2ae3413d4999c93
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 09:49:48 +0545

    mera

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import django.conf.urls.static import static
+from django.conf.urls.static import static
 
 
 urlpatterns = [
