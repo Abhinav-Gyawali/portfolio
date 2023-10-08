87fe9e85b051efbe390d93f3a142de266fd7374c
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 17:46:26 +0545

    k ma hency hzhztdfiydjeifie

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import dj_database_url
-from couple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
