92f540d1680f7ec49429f9c283b0d3fab15ff570
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 09:36:41 +0545

    meri aabai

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls'))
 ]
-urlpatterns += staticfiles_urlpatterns() # new
+urlpatterns += static (settings.STATIC_URL, document_root-settings.STATIC_ROOT) # new
 handler404 = 'pages.views.handling_404'
