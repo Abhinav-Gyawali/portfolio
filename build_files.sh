282664003992f63ce752ae04716cbad2df7a8355
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 20:10:24 +0545

    jdidnskxjdkzkabzusj

--- build_files.sh
+++ build_files.sh
@@ -9,3 +9,6 @@ python3.9 manage.py migrate --noinput
 echo "COLLECTING STATIC FILES"
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD SUCCESSFUL!"
+echo "SIGNING UP SUPERUSER"
+python manage.py createsuperuser --username=abhinav --email=publicgyawali@gmail.com --noinput
+echo "success"
