d54bca6b9d062f730c217266d4989865b35b9e32
Abhinav-Gyawali <publicgyawali@gmail.com>
2023-10-08 09:43:32 +0545

    meri

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
